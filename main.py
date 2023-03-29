ff3ae1f30a262d5186f19459d4ebe61f786de936
Séra Zoltán <dev.zoltan.sera@gmail.com>
2023-03-29 17:04:08 +0200

    Refactored and updated script

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 '''
-Scraping advertisement website for new ads with selenium
+Continuously scraping advertisement website for new ads with selenium
 '''
+
+
+import os
 import sys
 import time
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -21,20 +25,45 @@ class Scraper():
         self.frequency = int(args[4])
         self.repetitions = int(args[5])
         self.last_item_id = None
+        self.stored_data = { 'title': [], 'url': []}
+
 
     def run(self):
         '''
-        Creating log file, and calling scrape as many times as needed
+        Calling scrape as many times as needed, then calling export results
         '''
-        with open("logs.txt", "w", encoding="utf-8") as logfile:
-            print("Estimated run time: "+ str(self.frequency*self.repetitions) + " minutes.\n")
-            logfile.write("Scraping started at "+str(time.localtime()) + "\n")
+        print("Listening for new advertisements started.")
+        print("Estimated run time: "+ str(self.frequency*self.repetitions) + " minutes.")
+        # initial scrape, repetition count will start after this one
+        self.scrape()
+        for _ in range(self.repetitions):
+            print("Next check starts in " + str(self.frequency) + " minute(s).")
+            time.sleep(60*self.frequency)
+            print("Scraping started at "+str(time.localtime()))
+            self.scrape()
 
-            for _ in range(self.repetitions):
-                print("Scraping started at "+str(time.localtime()))
-                logfile.write("Scraping started at "+str(time.localtime()) + "\n")
-                self.scrape()
-                time.sleep(60*self.frequency)
+        self.export()
+
+
+    def export(self):
+        '''
+        Export new findings to csv
+        '''
+        if len(self.stored_data['title']) > 0:
+            data = pd.DataFrame({'title':self.stored_data['title'], 'url':self.stored_data['url']})
+            data.to_csv('results.csv', index=False)
+        else:
+            print("No new advertisements appeared during run time.")
+
+
+    def get_title_attr(self, item, attr):
+        '''
+        Returns attribute extracted from <a> element
+        found in parent with class item-title
+        '''
+        return item.find_element(By.CLASS_NAME, "item-title"). \
+            find_element(By.TAG_NAME, 'a').get_attribute(attr)
+
 
     def scrape(self):
         '''
@@ -47,39 +76,42 @@ class Scraper():
         searchbar.send_keys(Keys.RETURN)
         try:
             _element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, "main-box-footer")))
+                EC.presence_of_element_located((By.CLASS_NAME, "main-box-footer"))
+            )
+
             listings = driver.find_elements(
-                By.XPATH, '//*[contains(@class, "list-item")]')
+                By.XPATH, '//*[contains(@class, "list-item")]'
+            )
+
+            latest_list_id = listings[0].find_element(
+                By.XPATH, '//*[contains(@id, "listid")]'
+            ).get_attribute("id")
+
             if not self.last_item_id:
-                print("Found "+str(len(listings)) +
-                      " items on latest results page.")
-                for idx, item in enumerate(listings):
-                    if idx == 0:
-                        self.last_item_id = item.find_element(
-                            By.XPATH, '//*[contains(@id, "listid")]').get_attribute("id")
-                        print("Last item ID: " + self.last_item_id)
-                    print(item.find_element(
-                        By.CLASS_NAME, "item-title").
-                            find_element(By.TAG_NAME, 'a').get_attribute('text')
-                    )
+                print(
+                    "Found " + str(len(listings)) + " items on latest results page.",
+                    "Latest item ID: " + latest_list_id,
+                    sep=os.linesep
+                )
+                self.last_item_id = latest_list_id
+                for item in listings:
+                    print(self.get_title_attr(item, 'text'))
+
             else:
-                if listings[0].find_element(
-                        By.XPATH, '//*[contains(@id, "listid")]').get_attribute("id") \
-                        != self.last_item_id:
-                    self.last_item_id = listings[0].find_element(
-                        By.XPATH, '//*[contains(@id, "listid")]').get_attribute("id")
-                    print("UJ HIRDETES")
-                    print(listings[0].find_element(
-                        By.CLASS_NAME, "item-title").
-                        find_element(By.TAG_NAME, 'a').get_attribute('text')
-                    )
+                if  latest_list_id != self.last_item_id:
+                    self.last_item_id = latest_list_id
+                    print("New advertisement found!")
+                    self.stored_data['data'].append()
+                    self.stored_data['url'].append()
+                    print(self.get_title_attr(listings[0], 'text'))
+
                     php_url = "https://sorbusdigital.com/storage/sendnoti.php?mailto=" + \
-                    self.recipient + "&keyword=" + self.keyword + "&site=" + \
-                        self.site + "&url=" + \
-                        listings[0].find_element(
-                            By.CLASS_NAME, "item-title"). \
-                            find_element(By.TAG_NAME, 'a').get_attribute('href')
+                        self.recipient + "&keyword=" + self.keyword + "&site=" + \
+                        self.site + "&url=" + self.get_title_attr(listings[0], 'href')
+
                     driver.get(php_url)
+                else:
+                    print("No new ad appeared during this cycle.")
 
         finally:
             driver.quit()
